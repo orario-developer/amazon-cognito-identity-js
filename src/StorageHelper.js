4ead4c672d42bef844979a25075e50b28149441d
Tomokatsu Iguchi <tomo14916253@gmail.com>
2017-04-19 01:30:23 +0900

    [fix] cognito のStorageHelperをSQLiteを使うように変更、iOSユーザーかつ空き容量が少ないユーザーでログアウトが頻繁に起きていた問題修正

--- src/StorageHelper.js
+++ src/StorageHelper.js
@@ -15,8 +15,108 @@
  * limitations under the License.
  */
 
-let dataMemory = {};
+let sqlDataMemory = {},
+  _db = null;
 
+/** @class */
+class SQLiteStorage {
+
+  constructor(){
+    if(typeof PouchDB === 'undefined')
+      throw new Error('PouchDB is undefined.');
+
+    console.log('SQLiteStorage will be used.');
+    _db = new PouchDB('cognito', {adapter: 'websql'})
+    _db.info().then(this.init)
+
+  }
+
+  init(){
+    var _opt = {
+      include_docs: true,
+      attachments: true,
+      startkey: "cognito-", // Prefix search
+      endkey: "cognito-\uffff"
+    };
+    return _db.allDocs(_opt)
+      .then(function (res) {
+        sqlDataMemory = res.rows.map(function (row) {
+          return row.doc;
+        });
+      })
+  }
+
+
+  getItem(key){
+    var result = this.get(key);
+    return result ? result.value : null;
+  }
+
+  get(key){
+    var result = null;
+
+    for (var i = 0; i < sqlDataMemory.length; i++) {
+      if (sqlDataMemory[i].key == key) {
+        result = sqlDataMemory[i];
+        break;
+      }
+    }
+    console.log('============= get =================');
+    console.log('key', key);
+    console.log(JSON.stringify(result));
+    return result;
+  }
+
+  setItem(key, value) {
+    var data = this.get(key);
+    var isEditMode = !!data;
+    if (isEditMode) {
+      // 編集
+      console.log('!!!!!!!!!!! set !!!!!!!!!!!!!!!!');
+      console.log(JSON.stringify(data));
+      data.value = value;
+    } else {
+      // 新規作成
+      data = {
+        _id: "cognito-" + key,
+        type: "cognito",
+        key: key,
+        value: value
+      };
+    }
+
+    // DB
+    return _db.put(data)
+      .then(function (res) {
+        console.log("cognito: setItem 成功");
+        console.log(res);
+        data._rev = res.rev; // revの更新
+        if (!isEditMode) sqlDataMemory.push(data);
+      })
+  }
+
+  removeItem(key){
+    var data = this.get(key);
+    if(!data)
+      return;
+
+    data._deleted = true;
+
+    return _db.put(data).then(function (res) {
+      console.log("cognito: removeItem 成功");
+      console.log(JSON.stringify(res));
+
+      for (var i = 0; i < sqlDataMemory.length; i++) {
+        if (sqlDataMemory[i].key == key) {
+          sqlDataMemory.splice(i,1);
+          break;
+        }
+      }
+    });
+
+  }
+
+}
 /** @class */
 class MemoryStorage {
 
@@ -68,12 +168,21 @@ export default class StorageHelper {
    * @returns {object} the storage
    */
   constructor() {
-    try {
-      this.storageWindow = window.localStorage;
-      this.storageWindow.setItem('aws.cognito.test-ls', 1);
-      this.storageWindow.removeItem('aws.cognito.test-ls');
-    } catch (exception) {
-      this.storageWindow = MemoryStorage;
+    try{
+      // SQLite
+      this.storageWindow = new SQLiteStorage();
+    }catch(e){
+      try {
+        // LocalStorage
+        this.storageWindow = window.localStorage;
+        this.storageWindow.setItem('aws.cognito.test-ls', 1);
+        this.storageWindow.removeItem('aws.cognito.test-ls');
+        console.log('localStorage will be used.');
+      } catch (exception) {
+        // On Memory
+        this.storageWindow = MemoryStorage;
+        console.log('MemoryStorage will be used.');
+      }
     }
   }
 
